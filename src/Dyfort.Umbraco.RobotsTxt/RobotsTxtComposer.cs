3cd01090b5195dd19cf42894f19cbab8368deade
Yaroslav0709 <109355091+Yaroslav0709n@users.noreply.github.com>
2025-09-22 17:51:48 +0300

    Feature/read robots.txt from page (#1)
    * add support read robots.txt from umbraco page
    
    * check on null - umbracoContext.Content
    
    * update UmbracoPipelineFilter registration
    
    * update versions of umbraco packages

--- src/Dyfort.Umbraco.RobotsTxt/RobotsTxtComposer.cs
+++ src/Dyfort.Umbraco.RobotsTxt/RobotsTxtComposer.cs
@@ -3,25 +3,40 @@ using Microsoft.Extensions.DependencyInjection;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Web.Common.ApplicationBuilder;
+using Umbraco.Cms.Web.Common.Routing;
 
 namespace Dyfort.Umbraco.RobotsTxt
 {
     public class RobotsTxtComposer : IComposer
     {
         public void Compose(IUmbracoBuilder builder)
-        {        
-            
+        {
+            builder.Services.Configure<RobotsTxtSettings>(builder.Config.GetSection("RobotsTxt"));
+
+            builder.Services.Configure<UmbracoRequestOptions>(options =>
+            {
+                var allowList = new[] { "/robots.txt" };
+
+                options.HandleAsServerSideRequest = httpRequest =>
+                {
+                    foreach (string route in allowList)
+                    {
+                        if (httpRequest.Path.StartsWithSegments(route))
+                        {
+                            return true;
+                        }
+                    }
+
+                    return false;
+                };
+            });
+
             builder.Services.Configure<UmbracoPipelineOptions>(options =>
             {
-                options.AddFilter(new UmbracoPipelineFilter(
-                    "robots_txt",
-                    applicationBuilder => { },
-                    applicationBuilder => { },
-                    applicationBuilder =>
-                    {
-                        applicationBuilder.UseRobotsTxt(applicationBuilder.ApplicationServices.GetRequiredService<IWebHostEnvironment>());
-                    }
-                    ));
+                options.AddFilter(new UmbracoPipelineFilter("robots_txt")
+                {
+                    Endpoints = app => app.UseRobotsTxt(app.ApplicationServices.GetRequiredService<IWebHostEnvironment>())
+                });
             });
         }
     }
---
+++ src/Dyfort.Umbraco.RobotsTxt/RobotsTxtSettings.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Dyfort.Umbraco.RobotsTxt;
+
+public class RobotsTxtSettings
+{
+    public Guid? ContentKey { get; set; }
+    public string? FieldName { get; set; }
+}
